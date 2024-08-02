ce7e1936886f0035b15f6befdc59ef8640b83e83
Christyfernandes <christyabcd.05@gmail.com>
2024-08-02 17:55:49 +0530

    Task #0000: deleted message added for designation remove

--- project/ws/app/src/lib/routes/home/routes/designation/components/designations/designations.component.ts
+++ project/ws/app/src/lib/routes/home/routes/designation/components/designations/designations.component.ts
@@ -135,7 +135,7 @@ export class DesignationsComponent implements OnInit {
       } else {
         setTimeout(() => {
           this.getOrgReadData()
-        },         10000)
+        }, 10000)
       }
       // console.log('orgFramework Details', res)
     })
@@ -300,7 +300,7 @@ export class DesignationsComponent implements OnInit {
       this.designationsService.deleteDesignation(this.frameworkDetails.code, 'designation', requestBody).subscribe({
         next: res => {
           if (res) {
-            this.publishFrameWork()
+            this.publishFrameWork('delete')
           } else {
             this.showLoader = false
           }
@@ -314,12 +314,15 @@ export class DesignationsComponent implements OnInit {
     }
   }
 
-  publishFrameWork() {
+  publishFrameWork(action?: string) {
     const frameworkName = _.get(this.frameworkDetails, 'code')
     this.designationsService.publishFramework(frameworkName).subscribe({
       next: response => {
         if (response) {
           this.getFrameworkInfo(this.frameworkDetails.code)
+          if (action && action === 'delete') {
+            this.openSnackbar('Deleted Successfully')
+          }
         }
       },
       error: (error: HttpErrorResponse) => {
